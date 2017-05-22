31000b7e5d89a3c81071e67460d2e8b8eb703d7b
Ricky Brown <ricky@brilliantlabs.co>
2017-05-21 18:58:55 -0700

    shape up, strip out. use nonbox-client module for js

--- app/js/app.js
+++ app/js/app.js
@@ -1,4 +1,4 @@
-angular.module('app', ['ui.router', 'angular-electron']);
+angular.module('app', ['nonbox-client', 'angular-electron']);
 
 const app = angular.module('app');
 
@@ -7,8 +7,8 @@ app.run(function($rootScope, $state, Wifi, shell, remote){
   $rootScope.nbConnected = false;
 
   // nonbox router and api endpoints
-  $rootScope.nbServer = 'http://localhost:3000/'
-  $rootScope.nbApi = 'https://nonbox.co/'
+  $rootScope.nbServer = 'http://192.168.42.1:8080/'
+  $rootScope.nbApi    = 'https://nonbox.co/'
 
   // insternal state reference (via ng-click)
   $rootScope.sref = $state.go.bind($state);
