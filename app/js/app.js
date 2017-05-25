bc9fa609a57966dc0e95657993124552cefcb4c9
Ricky Brown <ricky@brilliantlabs.co>
2017-05-24 22:58:13 -0700

    keep custom app run function

--- app/js/app.js
+++ app/js/app.js
@@ -1,16 +1,10 @@
 angular.module('app', ['nonbox-client', 'angular-electron']);
 
 const app = angular.module('app');
+const templateDir = 'app/views';
 
 app.run(function($rootScope, $state, Wifi, shell, remote){
-  // assume nonbox isn't connected until known otherwise
-  $rootScope.nbConnected = false;
-
-  // nonbox router and api endpoints
-  $rootScope.nbServer = 'http://192.168.42.1:8080/'
-  $rootScope.nbApi    = 'https://nonbox.co/'
-
-  // insternal state reference (via ng-click)
+    // internal state reference (via ng-click)
   $rootScope.sref = $state.go.bind($state);
 
   // external link references (open in browser)
